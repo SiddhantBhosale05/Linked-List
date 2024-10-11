2160bcd6ec20d2ead2f4c3f225804f666e440781
Siddhant Bhosale <siddhant.bhosale05@gmail.com>
2024-10-11 21:33:15 +0530

    Add files via upload

---
+++ Add node at begenning.cpp
@@ -0,0 +1,53 @@
+//Name:Siddhant Kiran Bhosale
+//PRN:23070123038
+//Aim:Implemention of Linked List in a programto Add a node at the begenning of a Linked List
+
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int value;
+    Node* next;
+
+    Node(int data) {
+        value = data;
+        next = NULL;
+    }
+};
+
+void insertAtHead(Node* &head, int value) {
+    Node* new_node = new Node(value);
+    new_node->next = head;
+    head = new_node;
+}
+
+void display(Node* head) {
+    Node* temp = head;
+    while (temp != NULL) {
+        cout << temp->value << " -> ";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+
+int main() {
+    Node* head = NULL;
+
+    insertAtHead(head, 4);
+    display(head);
+
+    insertAtHead(head, 5);
+    display(head);
+
+    insertAtHead(head, 10);
+    display(head);
+
+    return 0;
+}
+
+
+/*Output:
+4 -> NULL
+5 -> 4 -> NULL
+10 -> 5 -> 4 -> NULL*/
---
+++ Linked List.cpp
@@ -0,0 +1,28 @@
+//Name:Siddhant Kiran Bhosale
+//PRN:23070123038
+//Aim:Implemention of Linked List in a program
+
+#include <iostream>
+using namespace std;
+
+
+    class Link{
+        public:
+        int data;
+        Link* next;
+        
+        Link(int num){
+        data = num;
+        next = NULL;
+    }
+};
+
+int main(){
+    Link* l1 = new Link(6);
+    cout<<l1 ->data<<" "<<l1->next;
+    return 0;
+}
+
+
+/*Output:
+6 0*/
